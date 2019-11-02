9d8208726b28787a25fd3c54f7098da9f6f56cce
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2019-11-02 05:03:03 +0100

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,6 @@
 
 // Load libraries
 
-require_once "../app/libraries/Core.php";
+require_once ("../app/libraries/Core.php");
 require_once "../app/libraries/Controller.php";
 require_once "../app/libraries/Database.php";
