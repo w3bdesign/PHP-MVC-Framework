92572fc0d238c890bd545ccd63fdf9cf4617355b
w3bdesign <>
2019-11-04 01:07:01 +0100

    Core update

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -19,6 +19,12 @@ class Core
 
     public function getUrl()
     {
-        // Todo: Get url parameter
+        // TODO Improve with Regex
+        if (isset($_GET["url"])) {
+            $url = rtrim($_GET["url"], "/");
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode("/", $url);
+            return $url;
+        }
     }
 }
--- public/index.php
+++ public/index.php
@@ -4,4 +4,4 @@ require_once "../app/bootstrap.php";
 // Initiate Core library
 
 $init = new Core;
-//print_r ("Welcome to index!");
+print_r("Welcome to public/index!");
