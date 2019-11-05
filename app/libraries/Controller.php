28ed7e93f26b47d323616370cecc916306fba7b5
w3bdesign <>
2019-11-05 02:16:11 +0100

    Update Controller.php

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -5,4 +5,23 @@
 */
 
 class Controller
-{ }
+{
+    // Load model
+    public function model($model)
+    {
+        // Require model file
+        require_once "../app/models" . $model . ".php";
+        // Init model
+        return new $model();
+    }
+
+    // Load view
+    public function view($view, $data = [])
+    {
+        // Check for view file
+        if (file_exists("../app/views/" . $view . ".php")) {
+            require_once "../app/views/" . $view . ".php";
+        }
+        die("View does not exist");
+    }
+}
