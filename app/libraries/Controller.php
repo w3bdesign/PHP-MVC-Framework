33f4c7947830115f9940c00ca552dddd5f6f7c3c
w3bdesign <daniel@test.no>
2019-11-05 18:23:51 +0100

    Composer update

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,26 +1,30 @@
 <?php
-/*
- Base controller
- Loads the models and the views
-*/
+
+/**
+ * Base controller
+ * Loads the models and the views
+ */
+
 
 class Controller
 {
+
     // Load model
     public function model($model)
     {
         // Require model file
-        require_once "../app/models" . $model . ".php";
+        include_once "../app/models" . $model . ".php";
         // Init model
         return new $model();
     }
 
     // Load view
+
     public function view($view, $data = [])
     {
         // Check for view file
         if (file_exists("../app/views/" . $view . ".php")) {
-            require_once "../app/views/" . $view . ".php";
+            include_once "../app/views/" . $view . ".php";
             return;
         }
         throw new Error("View " .  $view . " does not exist");
