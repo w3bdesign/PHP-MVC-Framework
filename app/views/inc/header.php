b49d6589badec827aa1d9f8b580a6774a2f11905
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2019-11-06 04:41:08 +0100

    Update header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo htmlspecialchars(SITENAME); ?></title>
+    <title><?php return SITENAME; ?></title>
 </head>
 
 <body>
