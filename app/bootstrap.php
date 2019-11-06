13a7637d3f719b9de249410d4029fa635edf5162
w3bdesign <daniel@test.no>
2019-11-06 04:17:46 +0100

    Add autoloader and views

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,11 +1,13 @@
 <?php
-
 // Load libraries
 // TODO Replace with autoloader?
 
 declare(strict_types=1);
 
+require_once "../app/config/config.php";
 
-require_once "../app/libraries/Core.php";
-require_once "../app/libraries/Controller.php";
-require_once "../app/libraries/Database.php";
+// Autoload core libraries
+
+spl_autoload_register(function ($className) {
+    require_once "../app/libraries/" . $className . ".php";
+});
---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+// App root
+define("APPROOT", dirname(dirname(__FILE__)));
+// URL root
+define("URLROOT", "http://localhost");
+// Site name
+define("SITENAME", "Site Name");
